9c2f92e813705a41e607bfc235a10b6f8b88617d
Mali Gaurav <maligaurav947@gmail.com>
2023-01-20 17:38:24 +0530

    Grid

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -11,84 +11,94 @@ export default function Product() {
       {
         id: 1,
         title: "Jam & Honey Boy's Regular Fit Shirt",
-        img: "https://m.media-amazon.com/images/I/919vpm+pvnL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/71BiQcsnzJL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 2,
         title: "Jam & Honey Boys Button Down Shirt",
-        img: "https://m.media-amazon.com/images/I/81+h4HYaxqL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/6134gSS8j6L._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 3,
         title: "Cherry Crumble California           Boy's Regular T-Shirt",
-        img: "https://m.media-amazon.com/images/I/A1LggkqYVRL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/81Lv0joTaRL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 4,
         title: "Oner Boys Jeans Regular fit Plain Jeans DN 003",
-        img: "https://m.media-amazon.com/images/I/A1Rhst1SymL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/61ID9h48xqL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 5,
         title: "",
-        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/19015668/2022/11/28/8d2bd4a4-8fd7-4c6b-9ab9-dadc486948841669632754722-US-Polo-Assn-Kids-Boys-Jackets-5291669632754306-1.jpg",
+        img: "https://m.media-amazon.com/images/I/61vF5YxGOPL._AC_UL480_QL65_.jpg",
       },
       {
         id: 6,
         title: "",
-        img: "https://m.media-amazon.com/images/I/81vWfvG6AqL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/61VtST6cgBL._AC_UL480_QL65_.jpg",
       },
       {
         id: 7,
         title: "",
-        img: "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/19582558/2022/8/22/7f4e61b5-3f14-4b4b-9e55-fb9df34722861661153905487ATUNGirlsPurpleAngrakhaKurtiwithTrousers1.jpg",
+        img: "https://m.media-amazon.com/images/I/61jSmXhGVdL._AC_UL480_QL65_.jpg",
       },
       {
         id: 8,
         title: "",
-        img: "https://m.media-amazon.com/images/I/91-qSvBudPL._AC_UL480_FMwebp_QL65_.jpg",
+        img: "https://m.media-amazon.com/images/I/51pUK5sBOSL._AC_UL480_FMwebp_QL65_.jpg",
+      },
+      {
+        id: 9,
+        title: "",
+        img: "https://m.media-amazon.com/images/I/41DlQF2nC9L._AC_UL480_QL65_.jpg",
+      },
+      {
+        id: 10,
+        title: "",
+        img: "https://m.media-amazon.com/images/I/61Lushz-26L._AC_UL480_FMwebp_QL65_.jpg",
       },
     ],
     Man: [
       {
         id: 1,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Pepe Jeans Men Jeans",
+        img: "https://m.media-amazon.com/images/I/811vZ9A3mqL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 2,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men's Regular Fit Button Down Shirt",
+        img: "https://m.media-amazon.com/images/I/71rON351gdL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 3,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men Hooded Sweatshirt",
+        img: "https://m.media-amazon.com/images/I/71bDzgBQPZL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 4,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men Casual Shorts",
+        img: "https://m.media-amazon.com/images/I/710GszJWzBS._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 5,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men's Slim Fit Button Down Shirt",
+        img: "https://m.media-amazon.com/images/I/815EsKQch7L._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 6,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men Black Hoodie",
+        img: "https://m.media-amazon.com/images/I/61+T54SqTpL._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 7,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men Jeans",
+        img: "https://m.media-amazon.com/images/I/41lGFbQtc5L._AC_UL480_FMwebp_QL65_.jpg",
       },
       {
         id: 8,
-        title: "",
-        img: "https://m.media-amazon.com/images/I/71-BLlWtxdL._UL1500_.jpg",
+        title: "Men Silver And Black Reversible Jacket",
+        img: "https://m.media-amazon.com/images/I/71zOvoKpVML._AC_UL480_FMwebp_QL65_.jpg",
       },
     ],
     Women: [
@@ -165,20 +175,21 @@ export default function Product() {
                 "ring-white ring-opacity-60 ring-offset-2 focus:outline-none focus:ring-2"
               )}
             >
-              <ul className="grid grid-cols-1 gap-5 lg:grid-cols-4 items-center justify-around ">
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-5 lg:grid-cols-5 md:grid-cols-3 items-center justify-around w-full">
                 {posts.map((post) => (
                   <>
                     <li
                       key={post.id}
-                      className="boxshadow relative rounded-md  bg-slate-300"
+                      className="boxshadow relative rounded-md"
                       style={{
-                        height: "520px",
+                        height: "100%",
+                        width: "100%",
                       }}
                     >
                       <img
                         src={post.img}
-                        className="object-cover object-top "
-                        style={{ width: "100%", height: "350px" }}
+                        className="object-contain"
+                        style={{ width: "100%", height: "250px" }}
                       />
                       <div className="p-3">
                         <div className=" flex items-center justify-between mt-1 text-yellow-500 gap-1 text-xl">
@@ -194,7 +205,7 @@ export default function Product() {
                         <ul className="mt-4 mb-4 grid  font-normal leading-4 text-gray-500">
                           <h2 className="text-base">{post.title}</h2>
                         </ul>
-                        <div className="flex items-center justify-between ">
+                        <div className="flex items-center justify-between">
                           <p className="text-xl font-extrabold">$21.05</p>
                           <button className="bg-emerald-500 p-2 rounded-md">
                             Add To Card
